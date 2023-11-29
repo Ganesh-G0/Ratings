382e8e69ed1f39727f263721f8b98ded632d2b49
Ganesh <121486938+Ganesh-G0@users.noreply.github.com>
2023-11-29 13:05:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+$(".one").click(() => { $("#ratings-given").text($('.one').text()) })
+$(".two").click(() => { $("#ratings-given").text($('.two').text()) })
+$(".three").click(() => { $("#ratings-given").text($('.three').text()) })
+$(".four").click(() => { $("#ratings-given").text($('.five').text()) })
+$(".five").click(() => { $("#ratings-given").text($('.five').text()) })
+
+
+$('#button').click(() => {
+    $('#ratings').addClass('d-none');
+    $('#mess').removeClass('d-none');
+})
