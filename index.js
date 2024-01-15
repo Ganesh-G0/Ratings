c2dcf0c3c701cbb7ea89a0216ba4500746012435
Ganesh <121486938+Ganesh-G0@users.noreply.github.com>
2024-01-15 14:29:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 $(".one").click(() => { $("#ratings-given").text($('.one').text()) })
 $(".two").click(() => { $("#ratings-given").text($('.two').text()) })
 $(".three").click(() => { $("#ratings-given").text($('.three').text()) })
-$(".four").click(() => { $("#ratings-given").text($('.five').text()) })
+$(".four").click(() => { $("#ratings-given").text($('.four').text()) })
 $(".five").click(() => { $("#ratings-given").text($('.five').text()) })
 
+let spanElement = $("#ratings-given");
+// console.log(spanElement.text().length)
 
 $('#button').click(() => {
-    $('#ratings').addClass('d-none');
-    $('#mess').removeClass('d-none');
+    if (spanElement.text().length !== 0) {
+            $('#ratings').addClass('d-none');
+            $('#message').removeClass('d-none');
+        }
+        else {
+            alert("Please Select a ratings from the list to Continue")
+        }
 })
