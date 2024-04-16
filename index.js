3d37de42681086c527b2e85785c7b0a4d6212a03
Ganesh <121486938+Ganesh-G0@users.noreply.github.com>
2024-04-16 12:27:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-$(".one").click(() => { $("#ratings-given").text($('.one').text()) })
-$(".two").click(() => { $("#ratings-given").text($('.two').text()) })
-$(".three").click(() => { $("#ratings-given").text($('.three').text()) })
-$(".four").click(() => { $("#ratings-given").text($('.four').text()) })
-$(".five").click(() => { $("#ratings-given").text($('.five').text()) })
+let ratings = $("#ratings-given");
 
-let spanElement = $("#ratings-given");
-// console.log(spanElement.text().length)
+$(".rate").click((e) => { ratings.text(e.target.innerText) });
 
-$('#button').click(() => {
-    if (spanElement.text().length !== 0) {
-            $('#ratings').addClass('d-none');
-            $('#message').removeClass('d-none');
-        }
-        else {
-            alert("Please Select a ratings from the list to Continue")
-        }
-})
+$('#submit').click(() => {
+    if (ratings.text().length != 0) {
+        $('#ratings').addClass('d-none');
+        $('#message').removeClass('d-none');
+    } else { 
+        alert("Please Rate Us, As Per Your Experience.") 
+    }
+});
